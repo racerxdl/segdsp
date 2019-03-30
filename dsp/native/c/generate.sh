e9d3bb2984abfb06b8154f0b2f503a3f25a82038
Lucas Teske <lucas@teske.net.br>
2019-03-30 17:12:48 -0300

    Added Arm64 generator script (although c2goasm does not support it)

--- dsp/native/c/generate.sh
+++ dsp/native/c/generate.sh
@@ -2,11 +2,12 @@
 
 ARCH=`uname -m`
 
-if [ "${ARCH}" = "x86_64" ] || [ "${ARCH}" = "i386" ]
+if [[ "${ARCH}" = "x86_64" ]] || [[ "${ARCH}" = "i386" ]]
 then
   echo "------------- x86 ------------"
   python generate_x86.py
   # ./generate_x86.sh
+# The issue here is that c2goasm does not support arm yet
 # elif [ "${ARCH}" = "aarch64" ] || [ "${ARCH}" = "arm64" ]
 # then
 #   echo "------------- arm64 -----------"
---
+++ dsp/native/c/generate_arm64.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from common import *
+
+mainArch = "arm64"
+
+outputFolder = "../%s" %mainArch
+
+subarchs = [
+  {
+    "name": "aarch64",
+    "subarchtitle": "AArch64",
+    "flags": "-target aarch64-none-eabi"
+  }
+]
+
+sources = [
+  {
+    "filename": "multiply_conjugate_native.c",
+    "function": "MultiplyConjugate",
+    "cFunction": "multiplyConjugate",
+  },
+  {
+    "filename": "multiply_conjugate_inline_native.c",
+    "function": "MultiplyConjugateInline",
+    "cFunction": "multiplyConjugateInline",
+  },
+  {
+    "filename": "dotprod_native_complex.c",
+    "function": "DotProductNativeComplex",
+    "cFunction": "dotProductComplex",
+  },
+  {
+    "filename": "dotprod_native_float.c",
+    "function": "DotProductNativeFloat",
+    "cFunction": "dotProductFloat",
+  },
+  {
+    "filename": "dotprod_native_complex_complex.c",
+    "function": "DotProductNativeComplexComplex",
+    "cFunction": "dotProductComplexComplex",
+  },
+  {
+    "filename": "rotate_complex.c",
+    "function": "RotateNativeComplex",
+    "cFunction": "rotateComplex",
+  },
+  {
+    "filename": "firfilter.c",
+    "function": "FirFilter",
+    "cFunction": "firFilter",
+  }
+]
+
+initFolders()
+
+for source in sources:
+  for subarch in subarchs:
+    Process(mainArch, source, subarch, outputFolder)
+
+initFolders()
+formatFiles()
